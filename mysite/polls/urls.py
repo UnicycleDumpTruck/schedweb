16995b36d3424ef3130ad99271a9ac42075a1728
BabyWrassler <junk@jeffhighsmith.com>
2020-10-10 18:37:54 -0400

    testing task time listing in view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
+    # ex: /polls/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # ex: /polls/tasks/
+    path("tasks/", views.tasks, name="tasks"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Task, Task_Type
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def tasks(request):
+    latest_task_list = Task.objects.order_by("start_time")[:5]
+    output = ", ".join([t.start_time for t in latest_task_list])
+    return HttpResponse(output)
