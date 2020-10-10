8d4a8cc46ce685617f9cdae313a275ac2362171b
BabyWrassler <junk@jeffhighsmith.com>
2020-10-10 17:22:21 -0400

    initial commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,51 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+# class Place(models.Model):
+#     short_name = models.CharField(max_length=40)
+#     long_name = models.CharField(max_length=100)
+
+
+class Task_Type(models.Model):
+    short_name = models.CharField(max_length=40)
+    long_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    chart_color = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.short_name
+
+
+class Task(models.Model):
+    task_type = models.ForeignKey(Task_Type, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    # location = models.ForeignKey(Task_Type, on_delete=models.CASCADE, null=True)
+    start_time = models.CharField(max_length=5)
+    end_time = models.CharField(max_length=5)
+    weekday = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.task_type} {self.weekday} {self.start_time}-{self.end_time}"
